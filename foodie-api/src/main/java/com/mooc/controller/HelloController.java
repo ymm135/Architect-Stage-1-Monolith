a33fff73bead8c9f1c0aa1b357c6ae01e8bb78f6
yangmingming <yangmingming@yunovo.cn>
2020-09-23 17:44:01 +0800

    [matrix]Update:HelloSpring

---
+++ foodie-api/src/main/java/com/mooc/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.mooc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello Spring5";
+    }
+}
